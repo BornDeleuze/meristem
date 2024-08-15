0ab0ebd581f0b148342cbad8d951925b8a161f5c
Matt Beaudoin <ehxtwo@gmail.com>
2024-08-14 22:38:08 -0700

    toggle function working

--- app/components/header-section.js
+++ app/components/header-section.js
@@ -3,10 +3,11 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class CounterComponent extends Component {
-
+  @tracked expandHamburger = true;
   
   @action
   toggleHamburger() {
+    this.expandHamburger = !this.expandHamburger;
    console.log('welp')
   }
 }
